f8aaa65aa894af45256feeab2028b5cab4a842fe
Han zhiqiang <hzq8710@gmail.com>
2026-04-09 01:06:26 +0800

    Update Launcher.kt

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Freya Arbjerg and contributors1
+ * Copyright (c) 2021 Freya Arbjerg and contributors11
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
